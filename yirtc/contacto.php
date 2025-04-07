58d169ee321af3fb38afb3d43450ecac6dc90451
David Acuña <davic1987@gmail.com>
2025-04-06 18:39:59 -0600

    primer avance

---
+++ yirtc/contacto.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST['name'];
+    $email = $_POST['email'];
+    $mensaje = $_POST['message'];
+
+    $para = "davic1987@gmail.com";
+    $asunto = "Nuevo mensaje de contacto";
+    $contenido = "Nombre: $nombre\nCorreo: $email\nMensaje:\n$mensaje";
+    $cabeceras = "From: $email";
+
+    if (mail($para, $asunto, $contenido, $cabeceras)) {
+        echo "Mensaje enviado correctamente.";
+    } else {
+        echo "Hubo un error al enviar el mensaje.";
+    }
+}
+?>
